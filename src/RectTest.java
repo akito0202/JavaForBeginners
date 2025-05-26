94e5298f0cabce6c38fd05651cc05626044e9478
mizonoakito <zeal24360075@fsgcl.jp>
2025-05-26 10:08:22 +0900

    setColor

--- src/RectTest.java
+++ src/RectTest.java
@@ -1,4 +1,18 @@
 
-public class RectTest {
+public class RectTest extends MyFrame{
+	public void run() {
+		    setColor(255,255,0);		
+			fillRect(30,40,50,100);
+		    setColor(0,255,255);
+			fillRect(130,40,50,100);
+			setColor(255,0,255);
+			fillRect(30,200,150,10);
+			setColor(0,0,0);
+			fillRect(30,100,150,10);
+			setColor(200,200,200);
+			fillRect(20,250,150,10);
+
+	}
+	
 
 }
--- src/Start.java
+++ src/Start.java
@@ -3,9 +3,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("プログラムを実行します");
+		System.out.println("aaaa");
 		MyFrame frame1=new RectTest();
-
 	}
 
 }
