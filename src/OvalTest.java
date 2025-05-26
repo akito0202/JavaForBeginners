5e5e7ffee77e87567524e7f60abf70d0a2ddd304
mizonoakito <zeal24360075@fsgcl.jp>
2025-05-26 10:51:25 +0900

    OvalTest

---
+++ src/OvalTest.java
@@ -0,0 +1,9 @@
+
+public class OvalTest extends MyFrame{
+	public void run() {
+		fillOval(30,40,50,100);
+		setColor(255,0,0);
+		fillOval(90,40,50,100);
+	}
+
+}
--- src/RectTest.java
+++ src/RectTest.java
@@ -1,18 +1,24 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
-		    setColor(255,255,0);		
-			fillRect(30,40,100,100);
-		    setColor(0,255,255);
-			fillRect(130,40,100,100);
-			setColor(255,0,255);
-			fillRect(230,40,100,100);
-			setColor(0,0,0);
-			fillRect(30,200,100,100);
-			setColor(200,200,200);
-			fillRect(130,200,100,100);
-			setColor(255,255,255);
-			fillRect(60,230,50,50);
+	  //  setColor(255,255,0);		
+			
+	//	fillRect(30,40,50,100);
+		//fillRect(30,40,100,100);
+	  //  setColor(0,255,255);
+		//fillRect(130,40,50,100);
+		//fillRect(130,40,100,100);
+		//setColor(255,0,255);
+		//fillRect(30,200,150,10);
+		//fillRect(230,40,100,100);
+		//setColor(0,0,0);
+		//fillRect(30,100,150,10);
+		//fillRect(30,200,100,100);
+		//setColor(200,200,200);
+		//fillRect(20,250,150,10);
+		//fillRect(130,200,100,100);
+		//setColor(255,255,255);
+		//fillRect(60,230,50,50);
 
 	}
 	
