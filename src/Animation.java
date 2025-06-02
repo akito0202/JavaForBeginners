c0103a3051abbc34d56a4b67344291729a6806f4
mizonoakito <zeal24360075@fsgcl.jp>
2025-06-02 12:12:11 +0900

    5-3

--- src/Animation.java
+++ src/Animation.java
@@ -3,41 +3,38 @@ public class Animation extends MyFrame{
 	public void run() {
 		int y=10;
 		int x=10;
-		while(x<=250) {
+		
+		int count=0;
+		
+		for(count=0;count<=3;count++) {
+		while(x<300) {
 			clear();
 			setColor(0,128,0);
 			fillRect(x,y+100,10,100);
-			x+=5;
-			y+=3;
+			
+			x+=10;
+			
 			
 			sleep(0.1);
 			
 			}
-			
-		
-		while(x>=30) {
+		while(x>=10) {
 			clear();
 			setColor(0,128,0);
 			fillRect(x,y+100,10,100);
 			
-			x-=3;
+			x-=10;
+			
 			
 			sleep(0.1);
 			
 			}
-		while(y>=0) {
-			clear();
-			setColor(0,128,0);
-			fillRect(x,y+100,10,100);
-			
-			x+=3;
-			y-=3;
-			
-			sleep(0.1);
+		
 		
 			
-	}
-
+		
+		
+}
 }
 }
 
